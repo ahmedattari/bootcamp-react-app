b1b2172034dee6bdf4a9c159e8bbaf91f2e1a31b
Ahmed Raza <turkahmedattari@gmail.com>
2021-02-07 03:50:51 +0500

    React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>
+      Asalam Walikum
+      hello world!!!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
